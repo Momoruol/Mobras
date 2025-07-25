a349e926bc471ee5debf5654f142437f330bfbc8
Joao Eduardo Amaral Dalle Molle <dudu280325@gmail.com>
2025-07-24 22:03:00 -0300

    Inclusão de restante das páginas, transição da navbar resolvida e drop down resolvido

--- script.js
+++ script.js
@@ -8,11 +8,9 @@ drop_click.addEventListener('click', drop_function);
 
 function nav_fix() {
 	nav.classList.toggle('scrolled', window.scrollY > 0);
-	nav.classList.toggle('fixed', window.scrollY > 0);
-	hero.classList.toggle('mt-20', window.scrollY > 0);
 }
 
 function drop_function() {
-	drop.classList.toggle('flex');
+	drop.classList.toggle('flex-drop');
 	drop.classList.toggle('hidden');
 }
